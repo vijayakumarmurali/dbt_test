2cb0561d4ac9a438928b9a9612501758f4719db8
VIJAYAKUMAR MURALI <kmvijayakumar89@gmail.com>
2024-02-14 04:09:15 +0000

    code updated

--- models/first_bdt_test.sql
+++ models/first_bdt_test.sql
@@ -1,8 +1,75 @@
-{% for i in range(10) %}
+{{ config(materialized='table') }}
 
-    select {{ i }} as number
-    {% if not loop.last %}
+select *
+    from (
+        
+
+    select 0 as number
+    
         union all
-    {% endif %}
+    
 
-{% endfor %}
+
+
+    select 1 as number
+    
+        union all
+    
+
+
+
+    select 2 as number
+    
+        union all
+    
+
+
+
+    select 3 as number
+    
+        union all
+    
+
+
+
+    select 4 as number
+    
+        union all
+    
+
+
+
+    select 5 as number
+    
+        union all
+    
+
+
+
+    select 6 as number
+    
+        union all
+    
+
+
+
+    select 7 as number
+    
+        union all
+    
+
+
+
+    select 8 as number
+    
+        union all
+    
+
+
+
+    select 9 as number
+    
+
+
+    ) as model_limit_subq
+    limit 500
